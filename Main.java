b141f7387f76be49a599315a3b3337ad2b03a2e2
zhiixuanxx <linaxuan1225@gmail.com>
2026-01-31 15:21:59 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import controller.AuthController;
+import model.User;
+import GUI.*;
+
+public class Main{
+    public static void main(String[] args){
+        new LoginGUI();
+    }
+}
+
+ 
